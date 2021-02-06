5df775ea5557a2efa8838aa1d0539046306d30d4
Bohdan-KL <bohdanklots@gmail.com>
2021-02-06 15:12:16 +0200

    Added files.

---
+++ cmake-build-debug/CMakeFiles/cursach2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cursach2.dir/main.c.obj"
+  "cursach2.exe"
+  "cursach2.exe.manifest"
+  "cursach2.pdb"
+  "libcursach2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cursach2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
